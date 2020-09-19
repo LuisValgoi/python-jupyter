98cd2be8b6c344672b5c86e085ba357ae05efef6
Luis Valgoi <luis.valgoi@sap.com>
2020-09-18 21:43:16 -0300

    feat: updated server and template

--- predia/api/srv.py
+++ predia/api/srv.py
@@ -1,13 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import os
 from flask import Flask, request, render_template, make_response
 from sklearn.externals import joblib
 
-
 app = Flask(__name__, static_url_path='/static')
-model = joblib.load('model.pkl')
-
-
+GB = joblib.load('model/GB.pkl')
+MLP = joblib.load('model/MLP.pkl')
+LSTM = joblib.load('model/LSTM.pkl')
 
 @app.route('/')
 def display_gui():
@@ -15,29 +16,38 @@ def display_gui():
 
 @app.route('/verificar', methods=['POST'])
 def verificar():
-	sexo = request.form['gridRadiosSexo']
-	dependentes = request.form['dependentes']
-	casado = request.form['gridRadiosCasado']
-	trabalho_conta_propria = request.form['gridRadiosTrabalhoProprio']
-	rendimento = request.form['rendimento']
-	educacao = request.form['educacao']
-	valoremprestimo = request.form['valoremprestimo']
-	teste = np.array([[sexo,casado,dependentes,educacao,trabalho_conta_propria,rendimento,valoremprestimo]])
-	
-	print(":::::: Dados de Teste ::::::")
-	print("Sexo: {}".format(sexo))
-	print("Numero de Dependentes: {}".format(dependentes))
-	print("Casado: {}".format(casado))
-	print("Educacao: {}".format(educacao))
-	print("Trabalha por conta propria: {}".format(trabalho_conta_propria))
-	print("Rendimento: {}".format(rendimento))
-	print("Valor do emprestimo: {}".format(valoremprestimo))
-	print("\n")
+	lstm_features = []
+	lstm_features.append(request.form['DATA_FESTIVA'])
+	lstm_features.append(request.form['FERIADO'])
+	lstm_features.append(request.form['QTD_CONCORRENTES'])
+	lstm_features.append(request.form['UMIDADE'])
+	lstm_features.append(request.form['VENDAS_ONTEM'])
+	lstm_y_pred = lstm.predict(scaler.transform([lstm_features])).round().astype(int)[0]
 
-	qtd_almoco = model.predict(teste)[0]
-	print("Quantidade de Almoço: {}".format(str(qtd_almoco)))
+	gb_features = []
+	gb_features.append(request.form['ALTA_TEMPORADA'])
+	gb_features.append(request.form['DATA_FESTIVA'])
+	gb_features.append(request.form['FERIADO'])
+	gb_features.append(request.form['QTD_CONCORRENTES'])
+	gb_features.append(request.form['VENDAS_ONTEM'])
+	gb_y_pred = gb.predict(scaler.transform([gb_features])).round().astype(int)[0]
 
-	return render_template('template.html',qtd_almoco=str(qtd_almoco))
+	mlp_features = []
+	mlp_features.append(request.form['DATA_FESTIVA'])
+	mlp_features.append(request.form['FERIADO'])
+	mlp_features.append(request.form['POS_DATA_FESTIVA'])
+	mlp_features.append(request.form['TEMPERATURA'])
+	mlp_features.append(request.form['UMIDADE'])
+	mlp_features.append(request.form['VENDAS_ONTEM'])
+	mlp_y_pred = mlp.predict(scaler.transform([mlp_features])).round().astype(int)[0]
+
+	qtd_almoco_lstm = LSTM.predict(teste)[0]
+	qtd_almoco_gb = GB.predict(teste)[0]
+	qtd_almoco_mlp = MLP.predict(teste)[0]
+	qtd_almoco_ensemble = (qtd_almoco_lstm + qtd_almoco_gb + qtd_almoco_mlp) / 3
+	print(qtd_almoco_ensemble)
+
+	return render_template('template.html',qtd_almoco=str(qtd_almoco_ensemble))
 
 if __name__ == "__main__":
         port = int(os.environ.get('PORT', 5500))
